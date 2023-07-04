8b12b9bdfc84f9284a57538a2d03b581b800d191
Eran Cohen <eranco@redhat.com>
2023-07-04 14:57:00 +0300

    Update all internal IP related scripts to use 192.168.127.10

--- bake/dispatcher-pre-up-internal-ip.sh
+++ bake/dispatcher-pre-up-internal-ip.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 # This add the internal IP to the external `br-ex` bridge used by OVS:
-internal_ip="192.168.126.10/24"
+internal_ip="192.168.127.10/24"
 nmcli connection modify br-ex +ipv4.addresses "${internal_ip}" ipv4.method auto
 ip addr add "${internal_ip}" dev br-ex
